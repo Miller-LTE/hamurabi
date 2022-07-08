cc15048070bd6db70c2a359acd8e269dea7ffc4b
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-08 17:29:14 -0400

    setup base Hammurabi class

--- src/main/java/Hammurabi/Hammurabi.java
+++ src/main/java/Hammurabi/Hammurabi.java
@@ -1,7 +1,19 @@
 package Hammurabi;
+import com.sun.tools.jdeprscan.scan.Scan;
+
 import java.util.Random;
 import java.util.Scanner;
 
 
 public class Hammurabi {
+    Random rand = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args){
+        new Hammurabi().playGame();
+    }
+
+    void playGame(){
+
+    }
 }
