3faa2b657fa7bf3f3802cedd53d8f7dcf8119375
Troy <troy@ZipcodersMBP28.fios-router.home>
2022-07-08 18:30:24 -0400

    test

--- src/main/java/Hammurabi/Hammurabi.java
+++ src/main/java/Hammurabi/Hammurabi.java
@@ -1,19 +1,21 @@
-package Hammurabi;
+package Hamurabi;
 import com.sun.tools.jdeprscan.scan.Scan;
 
 import java.util.Random;
 import java.util.Scanner;
 
 
-public class Hammurabi {
+public class Hamurabi {
     Random rand = new Random();
     Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args){
-        new Hammurabi().playGame();
+        new Hamurabi().playGame();
     }
 
     void playGame(){
+
+
         public int askHowManyAcresToBuy(int price, int bushels){
 
         }
