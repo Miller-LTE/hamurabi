de4f2e7a6dc001a3411526940052f6e5810591f0
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-08 18:16:18 -0400

    methods

--- src/main/java/Hammurabi/Hammurabi.java
+++ src/main/java/Hammurabi/Hammurabi.java
@@ -1,7 +1,21 @@
-package Hammurabi;
+package hamurabi.src.main.java.Hammurabi;
+
 import java.util.Random;
 import java.util.Scanner;
 
-
 public class Hammurabi {
+    Random rand = new Random();  // this is an instance variable
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) { // required in every Java program
+        new Hammurabi().playGame();
+    }
+
+    void playGame() {
+        // declare local variables here: grain, population, etc.
+        // statements go after the declations
+    }
+
+    //other methods go here
+
 }
