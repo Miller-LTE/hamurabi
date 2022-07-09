58108580528756c99660a29141805bf0474396db
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-08 23:09:18 -0400

    buy method works

--- src/main/java/Hammurabi/Hammurabi.java
+++ src/main/java/Hammurabi/Hammurabi.java
@@ -1,23 +1,22 @@
 
-package Hamurabi;
-import com.sun.tools.jdeprscan.scan.Scan;
+package Hammurabi;
 
 import java.util.Random;
 import java.util.Scanner;
 
 
-public class Hammurabi {
-    Random rand = new Random();  // this is an instance variable
-    Scanner scanner = new Scanner(System.in);
+//public class Hammurabi {
+//    Random rand = new Random();  // this is an instance variable
+//    Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) { // required in every Java program
-        new Hammurabi().playGame();
-    }
-
-    void playGame() {
-        // declare local variables here: grain, population, etc.
-        // statements go after the declations
-    }
+//    public static void main(String[] args) { // required in every Java program
+//        new Hammurabi().playGame();
+//    }
+//
+//    void playGame() {
+//        // declare local variables here: grain, population, etc.
+//        // statements go after the declations
+//    }
 
     //other methods go here
 
@@ -46,32 +45,43 @@ public class Hammurabi {
     }
 
     void playGame(){
+        while (years <= 10){
         System.out.println("O great Harambe!\n" +
                 "You are in year " + years + " of your ten year rule.\n" +
                 "In the previous year " + starved + " people starved to death.\n" +
                 "In the previous year " + immigrants + " people entered the kingdom.\n" +
                 "The population is now " + population + ".\n" +
                 "We harvested " + bushels + " bushels at " + harvest + " bushels per acre.\n" +
-                "Rats destroyed " + grainEatenByRats + " bushels, leaving 2800 bushels in storage.\n" +
-                "The city owns 1000 acres of land.\n" +
+                "Rats destroyed " + grainEatenByRats + " bushels, leaving " + bushels + " bushels in storage.\n" +
+                "The city owns " + acresOwned + " acres of land.\n" +
                 "Land is currently worth " + price + " bushels per acre.\n");
-
-        while (years <= 10){
             years++;
+            int temp = 0;
             askHowManyAcresToBuy(price, bushels);
+            askHowManyAcresToSell(acresOwned);
 
+            break;
         }
     }
 
     private int askHowManyAcresToBuy(int price, int bushels){
         System.out.print("How many acres of land would you like to buy? ");
-        bushels = scanner.nextInt();
-        return price;
+        int input = scanner.nextInt();
+        if(bushels<0){
+            System.out.println("Does not work!");
+        } else {
+            bushels-=price*input;
+        }
+        acresOwned+= input;
+        System.out.println(bushels);
+        return input;
     }
 
     private int askHowManyAcresToSell(int acres){
         System.out.print("How many acres of land would you like to sell? ");
-        acres = scanner.nextInt();
+        int input = scanner.nextInt();
+        System.out.println(bushels);
+
         return acres;
     }
 
@@ -83,5 +93,5 @@ public class Hammurabi {
 
 
 }
-}
+
 
